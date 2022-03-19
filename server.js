a10484470d70df9173da3d7ded7ec7e599614336
zayarwin <zayarwin7751@gmail.com>
2022-03-19 23:19:11 +0630

    about-basic-route

--- server.js
+++ server.js
@@ -4,21 +4,33 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
   //set response header's content-type
   res.setHeader("Content-Type", "text/html");
-  fs.readFile(
-    "./views/index.html",
-    (err, data) => {
-      if (err) {
-        console.log(err);
-        //to end the page loading
-        res.end();
-      }
-      if (data) {
-        //res.write(data);
-        //if it a single data to send to client you can write in end method
-        res.end(data);
-      }
+
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      //to end the page loading
+      res.end();
     }
-  );
+    if (data) {
+      //res.write(data);
+      //if it a single data to send to client you can write in end method
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
