eb09e098df88019ee25dad7876105ddf6e69b520
zayarwin <zayarwin7751@gmail.com>
2022-03-20 21:52:08 +0630

    about-ejs-engine

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 const express = require("express");
+const req = require("express/lib/request");
 
 //express app
 const app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
 //listem for request
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", {
-    root: __dirname,
-  });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  //we can pass the data to ejs template from render's send parameter
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", {
-    root: __dirname,
-  });
+  res.render("about", { title: "About" });
 });
 
-//reidrect
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create" });
 });
 
 //404
@@ -26,11 +43,7 @@ app.get("/about-us", (req, res) => {
 //express route every render top to bottom
 //so this 404 middleware must be the end of the route;
 app.use((req, res) => {
-  res
-    .send(404)
-    .sendFile("./views/404.html", {
-      root: __dirname,
-    });
+  res.render("404", { title: "404" });
 });
 
 app.listen(3000, () => {
